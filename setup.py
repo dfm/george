4e11060da0abab146c88b7376fba3adb62578f47
Dan F-M <danfm@nyu.edu>
2013-02-28 10:02:10 -0500

    adding Pypi stuff to setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python
 
 import os
+import sys
+
+if "publish" in sys.argv[-1]:
+    os.system("python setup.py sdist upload")
+    sys.exit()
 
 try:
     from setuptools import setup, Extension
@@ -24,7 +29,19 @@ setup(
     version="0",
     author="Daniel Foreman-Mackey",
     author_email="danfm@nyu.edu",
+    url="https://github.com/dfm/george",
     packages=["george"],
     ext_modules=[gp_ext],
     description="Blazingly fast Gaussian Processes.",
+    long_description=open("README.rst").read(),
+    package_data={"": ["README.rst", "LICENSE.rst"]},
+    include_package_data=True,
+    classifiers=[
+        # "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+    ],
 )
