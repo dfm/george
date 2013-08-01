5de023766890f6f09a69658953899b29cc23aec6
Dan F-M <danfm@nyu.edu>
2013-08-01 15:07:55 -0400

    no more C kernels

--- george/kernels.h
+++
@@ -1,9 +0,0 @@
-#ifndef _KERNELS_H_
-#define _KERNELS_H_
-
-double isotropicKernel (double x1, double x2, int npars, double *pars);
-void gradIsotropicKernel (double x1, double x2, int npars, double *pars,
-                          double *dkdt);
-
-#endif
-// /_KERNELS_H_
