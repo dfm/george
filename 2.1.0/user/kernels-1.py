2ec813bf84d027e9487528aa67743272dc9b0bc3
Dan F-M <danfm@nyu.edu>
2014-07-16 13:49:42 +0200

    updating docs

--- 2.1.0/user/kernels-1.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as pl
-from george.kernels import ExpSquaredKernel
-r = np.linspace(0, 3)
-kr = ExpSquaredKernel(1.0).get_value(r * r)
-pl.plot(r, kr, "k", lw=2)
-pl.show()
