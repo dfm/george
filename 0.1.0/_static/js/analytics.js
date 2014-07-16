436784e1e15b1059e67e1dcf771a0bcdca51d8e9
Dan F-M <danfm@nyu.edu>
2014-07-16 16:07:52 +0200

    universal analytics

--- 0.1.0/_static/js/analytics.js
+++ 0.1.0/_static/js/analytics.js
@@ -4,4 +4,5 @@ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
 ga('create', 'UA-22909046-1', 'auto');
+ga('require', 'displayfeatures');
 ga('send', 'pageview');
