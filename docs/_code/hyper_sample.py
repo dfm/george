2e2f924caa8d2c514bc828b382d1f5b25a5c3848
Dan F-M <danfm@nyu.edu>
2014-07-16 10:59:22 +0000

    MCMC results

--- docs/_code/hyper_sample.py
+++ docs/_code/hyper_sample.py
@@ -8,6 +8,7 @@ http://www.gaussianprocess.org/gpml/
 
 from __future__ import division, print_function
 
+import sys
 import time
 import emcee
 import numpy as np
@@ -44,6 +45,8 @@ print(time.time() - s)
 # Define the probabilistic model.
 def lnprob(p):
     # Trivial improper prior: uniform in the log.
+    if np.any((-10 > p) + (p > 10)):
+        return -np.inf
     lnprior = 0.0
 
     # Update the kernel and compute the lnlikelihood.
@@ -53,23 +56,21 @@ def lnprob(p):
 
 # Set up the sampler and initialize the walkers.
 nwalkers, ndim = 36, len(kernel)
-p0 = [np.log(kernel.pars) + 1e-8 * np.random.randn(ndim)
-      for i in range(nwalkers)]
+
+if len(sys.argv) > 1:
+    chain, _, _ = pickle.load(open(sys.argv[1], "rb"))
+    p0 = chain[:, -1, :]
+else:
+    p0 = [np.log(kernel.pars) + 1e-4 * np.random.randn(ndim)
+          for i in range(nwalkers)]
 
 sampler = emcee.EnsembleSampler(nwalkers, ndim, lnprob, pool=Pool())
 print("Sampling...")
 s = time.time()
-for i in range(10):
+for i in range(20):
     print(i+1)
     p0, _, _ = sampler.run_mcmc(p0, 200)
     pickle.dump((sampler.chain, sampler.lnprobability, gp),
                 open("hyper-results-{0}.pkl".format(i), "wb"), -1)
 
 print("Finished {0}".format(time.time() - s))
-
-# Plot the traces.
-samples = sampler.chain
-for i in range(samples.shape[2]):
-    pl.clf()
-    pl.plot(samples[:, :, i].T, color="k", alpha=0.5)
-    pl.savefig("time/{0:03d}.png".format(i))
