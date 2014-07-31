e7f747b66bf132de6a5db1fbd70b08309e83f91b
Dan F-M <danfm@nyu.edu>
2014-08-01 01:45:03 +0200

    bumping version number

--- george/__init__.py
+++ george/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-__version__ = "0.1.1-dev"
+__version__ = "0.1.1"
 
 try:
     __GEORGE_SETUP__
--- george/testing/__init__.py
+++ george/testing/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
 
-__all__ = ["test_basic", "test_kernels"]
+__all__ = ["test_basic", "test_kernels", "test_tutorial"]
 
-from . import test_basic, test_kernels
+from . import test_basic, test_kernels, test_tutorial
