Fixing gradient bug (#13)

george/gp.py
@@ -236,7 +236,8 @@ class GP(object):
 
         # Pre-compute some factors.
         alpha = self.solver.apply_inverse(r)
-        Kg = self.kernel.gradient(self._x)
+        Kg = np.ascontiguousarray(self.kernel.gradient(self._x).T,
+                                  dtype=np.float64)
 
         # Loop over dimensions and compute the gradient in each one.
         g = np.empty(len(Kg))

george/testing/test_kernels.py
@@ -50,7 +50,7 @@ def do_kernel_t(kernel, N=20, seed=123, eps=1.32e-7):
         kernel[i] += eps
         g0 = 0.5 * (kp - km) / eps
         assert np.allclose(g1[:, :, i], g0), \
-            "Python gradient computation failed in dimension {0}".format(i)
+            "Gradient computation failed in dimension {0}".format(i)
 
 
 #