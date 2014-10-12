407d8159ef6e896471425472bf031e1e080d5073
Dan F-M <danfm@nyu.edu>
2014-10-12 18:54:38 -0400

    some progress on solver docs

--- docs/user/solvers.rst
+++ docs/user/solvers.rst
@@ -9,6 +9,8 @@ There are currently two different GP solvers included with George using
 different libraries for doing linear algebra.
 Both of the solvers implement the same API and should (up to some tolerance)
 give the same answers on the same datasets.
+A solver
+
 The first solver :class:`GP` is a basic implementation using NumPy and SciPy
 so it will be linked to whichever LAPACK/BLAS is used by those libraries.
 This solver is very robust and it should be fairly efficient on small to
@@ -21,15 +23,15 @@ This solver is built using Sivaram Amambikasaran's `HODLR library
 `here <http://arxiv.org/abs/1403.6015>`_.
 
 
-.. Basic Solver
-.. ------------
+Basic Solver
+------------
 
-.. .. autoclass:: george.GP
-..    :inherited-members:
+.. autoclass:: george.BasicSolver
+   :inherited-members:
 
 
-.. HODLR Solver
-.. ------------
+HODLR Solver
+------------
 
-.. .. autoclass:: george.HODLRGP
-..    :inherited-members:
+.. autoclass:: george.HODLRSolver
+   :inherited-members:
--- george/basic.py
+++ george/basic.py
@@ -9,12 +9,31 @@ from scipy.linalg import cholesky, cho_solve
 
 
 class BasicSolver(object):
+    """
+    This is the most basic solver for use by
+
+    :param kernel:
+        A subclass of :class:`Kernel` implementing a kernel function.
+
+    """
 
     def __init__(self, kernel):
         self.kernel = kernel
         self.computed = False
 
     def compute(self, x, yerr):
+        """
+        Compute and factorize the covariance matrix.
+
+        :param x: ``(nsamples, ndim)``
+            The independent coordinates of the data points.
+
+        :param yerr: (optional) ``(nsamples,)`` or scalar
+            The Gaussian uncertainties on the data points at coordinates
+            ``x``. These values will be added in quadrature to the diagonal of
+            the covariance matrix.
+
+        """
         # Compute the kernel matrix.
         K = self.kernel.value(x)
         K[np.diag_indices_from(K)] += yerr ** 2
@@ -25,7 +44,13 @@ class BasicSolver(object):
         self.computed = True
 
     def apply_inverse(self, y, in_place=False):
+        """
+
+        """
         return cho_solve(self._factor, y, overwrite_b=in_place)
 
     def apply_sqrt(self, r):
+        """
+
+        """
         return np.dot(r, self._factor[0])
