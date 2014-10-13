a93d0f4930892abe19429e371298b14a60fbfda2
Dan F-M <danfm@nyu.edu>
2014-10-13 12:20:15 -0400

    documentation for the HODLR solver

--- george/basic.py
+++ george/basic.py
@@ -10,7 +10,7 @@ from scipy.linalg import cholesky, cho_solve
 
 class BasicSolver(object):
     """
-    This is the most basic solver for use by
+    This is the most basic solver built using :func:`scipy.linalg.cholesky`.
 
     :param kernel:
         A subclass of :class:`Kernel` implementing a kernel function.
@@ -19,7 +19,34 @@ class BasicSolver(object):
 
     def __init__(self, kernel):
         self.kernel = kernel
-        self.computed = False
+        self._computed = False
+        self._log_det = None
+
+    @property
+    def computed(self):
+        """
+        A flag indicating whether or not the covariance matrix was computed
+        and factorized (using the :func:`compute` method).
+
+        """
+        return self._computed
+
+    @computed.setter
+    def computed(self, v):
+        self._computed = v
+
+    @property
+    def log_determinant(self):
+        """
+        The log-determinant of the covariance matrix. This will only be
+        non-``None`` after calling the :func:`compute` method.
+
+        """
+        return self._log_det
+
+    @log_determinant.setter
+    def log_determinant(self, v):
+        self._log_det = v
 
     def compute(self, x, yerr):
         """
@@ -44,13 +71,29 @@ class BasicSolver(object):
         self.computed = True
 
     def apply_inverse(self, y, in_place=False):
-        """
+        r"""
+        Apply the inverse of the covariance matrix to the input by solving
+
+        .. math::
+
+            C\,x = b
+
+        :param y: ``(nsamples,)`` or ``(nsamples, nrhs)``
+            The vector or matrix :math:`b`.
+
+        :param in_place: (optional)
+            Should the data in ``y`` be overwritten with the result :math:`x`?
 
         """
         return cho_solve(self._factor, y, overwrite_b=in_place)
 
     def apply_sqrt(self, r):
         """
+        Apply the Cholesky square root of the covariance matrix to the input
+        vector or matrix.
+
+        :param r: ``(nsamples,)`` or ``(nsamples, nrhs)``
+            The input vector or matrix.
 
         """
         return np.dot(r, self._factor[0])
--- george/hodlr.pyx
+++ george/hodlr.pyx
@@ -26,6 +26,27 @@ def _rebuild(kernel_spec, nleaf, tol):
 
 
 cdef class HODLRSolver:
+    """
+    A solver using Sivaram Amambikasaran's `HODLR library
+    <https://github.com/sivaramambikasaran/HODLR>`_ that implements a
+    :math:`\mathcal{O}(N\,\log^2 N)` direct solver for dense matrices as
+    described `here <http://arxiv.org/abs/1403.6015>`_.
+
+    :param kernel:
+        A subclass of :class:`Kernel` implementing a kernel function.
+
+    :param nleaf: (optional)
+        The size of the smallest matrix blocks. When the solver reaches this
+        level in the tree, it directly solves these systems using Eigen's
+        Cholesky implementation. (default: ``100``)
+
+    :param tol: (optional)
+        A tuning parameter used when factorizing the matrix. The conversion
+        between this parameter and the precision of the results is problem
+        specific but if you need more precision, try deceasing this number
+        (at the cost of a longer runtime). (default: ``1e-12``)
+
+    """
 
     cdef object kernel_spec
     cdef kernels.Kernel* kernel
@@ -33,8 +54,7 @@ cdef class HODLRSolver:
     cdef unsigned int nleaf
     cdef double tol
 
-    def __cinit__(self, kernel_spec, unsigned int nleaf=100, double tol=1e-12,
-                  rebuild=False):
+    def __cinit__(self, kernel_spec, unsigned int nleaf=100, double tol=1e-12):
         self.kernel_spec = kernel_spec
         self.kernel = kernels.parse_kernel(kernel_spec)
         self.solver = new Solver(self.kernel, nleaf, tol)
@@ -50,6 +70,26 @@ cdef class HODLRSolver:
 
     def compute(self, np.ndarray[DTYPE_t, ndim=2] x,
                 np.ndarray[DTYPE_t, ndim=1] yerr, seed=None):
+        """
+        compute(x, yerr, seed=None)
+        Compute and factorize the covariance matrix.
+
+        :param x: ``(nsamples, ndim)``
+            The independent coordinates of the data points.
+
+        :param yerr: (optional) ``(nsamples,)`` or scalar
+            The Gaussian uncertainties on the data points at coordinates
+            ``x``. These values will be added in quadrature to the diagonal of
+            the covariance matrix.
+
+        :param seed: (optional)
+            There is a stochastic component in the HODLR factorization step.
+            Use this parameter (it should be an integer) to seed this random
+            step and ensure deterministic results. Normally the randomization
+            shouldn't make a big difference but as the matrix becomes poorly
+            conditioned, it will have a larger effect.
+
+        """
         # Check the input dimensions.
         cdef unsigned int n = x.shape[0]
         cdef unsigned int ndim = x.shape[1]
@@ -67,14 +107,39 @@ cdef class HODLRSolver:
             raise np.linalg.LinAlgError(info)
 
     property log_determinant:
+        """
+        The log-determinant of the covariance matrix. This will only be
+        non-``None`` after calling the :func:`compute` method.
+
+        """
         def __get__(self):
             return self.solver.get_log_determinant()
 
     property computed:
+        """
+        A flag indicating whether or not the covariance matrix was computed
+        and factorized (using the :func:`compute` method).
+
+        """
         def __get__(self):
             return bool(self.solver.get_computed())
 
     def apply_inverse(self, y0, in_place=False):
+        """
+        apply_inverse(y, in_place=False)
+        Apply the inverse of the covariance matrix to the input by solving
+
+        .. math::
+
+            C\,x = b
+
+        :param y: ``(nsamples,)`` or ``(nsamples, nrhs)``
+            The vector or matrix :math:`b`.
+
+        :param in_place: (optional)
+            Should the data in ``y`` be overwritten with the result :math:`x`?
+
+        """
         # Coerce the input array into the correct format.
         cdef np.ndarray[DTYPE_t, ndim=2] y
         if len(y0.shape) == 1:
@@ -96,5 +161,10 @@ cdef class HODLRSolver:
         return alpha.reshape(y0.shape)
 
     def apply_sqrt(self, y0):
+        """
+        apply_sqrt(r)
+        This method is not implemented by this solver yet.
+
+        """
         raise NotImplementedError("The sqrt function isn't available in "
                                   "the HODLR solver yet")
